eb8e406d68af4518428114859476babab8bdaae6
Rafael Marcos <rafix995@gmail.com>
2023-12-16 21:30:26 +0100

    TextView aplicada

---
+++ app/src/main/java/ao/rafaelmarcos/avaliacaowidgets/MainActivity.java
@@ -0,0 +1,26 @@
+package ao.rafaelmarcos.avaliacaowidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        return;
+    }
+}
