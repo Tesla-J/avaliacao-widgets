85fd021b496e2e4635f3c5a3a60e87a275c42c32
Rafael Marcos <rafix995@gmail.com>
2023-12-18 23:46:00 +0100

    Not progress dialog, but progress bar

--- app/src/main/java/ao/rafaelmarcos/avaliacaowidgets/InfiniteLoadingActivity.java
+++ app/src/main/java/ao/rafaelmarcos/avaliacaowidgets/InfiniteLoadingActivity.java
@@ -10,5 +10,22 @@ public class InfiniteLoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infinite_loading);
+
+        /*
+        * Antes de mais nada, admito que eu fiz essa activity para
+        * trollar, mas indo para oque importa:
+        *
+        * Não utilizei o ProgressDialog porque na documentação diz
+        * que ela está obsoleta porque não permite a interação com
+        * a app enquando ela está visível. Por isso optei por algo
+        * actualizado e que será mais útil ter conhecimendo.
+        *
+        * Nota: eu utilizei o ProgressBar para progressos indeterminados,
+        * apenas para fins ilustrativos.
+        *
+        * //TODO determined progress counter with user input.
+        * //TODO TaskCheduling and Multithread counter (1 per second)
+        * //TODO use a service to manage the progressBar
+        */
     }
 }
